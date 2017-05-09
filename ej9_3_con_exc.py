1af86f4db863cb0e9339bac8185bd0baccd8b861
elaynealcala <elaynealcala@gmail.com>
2017-05-09 14:08:00 -0400

    Ejercicion 9 con manejo de excepciones

---
+++ ej9_3_con_exc.py
@@ -0,0 +1,36 @@
+# Descripcion: Este programa pide a un usuario adivinar un numero entre el 1 y el 9, ambos incluidos
+# Entrada: Numero entre el uno y el nueve
+# Salida:  El numero es mayor, menor o igual
+# Autor:   EALCALA
+# Fecha:  04.05.2017
+# Version: 2.0
+#Plataforma: Python v2.7
+
+import random
+
+a = int(random.randrange(1, 9))
+n = 0
+c = 0
+
+
+while n != a and n != "fin":
+ n = input("Adivine un numero entre el 1 y el 9, ambos incluidos, o teclee fin para salir: ")
+
+ if n == fin:
+  break
+
+ try:
+  n = int(n)
+ except ValueError:
+  print("El valor no es un numero")
+
+ c += 1
+
+ if n>a:
+  print("su numero es mayor al escogido!")
+ elif n<a:
+  print("su numero es menor al elegido")
+ else:
+  print("Felicidades, usted ha adivinado!")
+  print("numero de intentos")
+  print c
