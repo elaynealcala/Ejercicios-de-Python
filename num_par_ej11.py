447f3ffccb890c7f9a810afd59322781d13de443
elaynealcala <elaynealcala@gmail.com>
2017-05-09 13:49:36 -0400

    Ejercicio 11 con manejo de excepciones

---
+++ num_par_ej11.py
@@ -0,0 +1,20 @@
+# Descripcion: Programa que pide al usuario un numero x y le devuelve de respuesta si el numero es primo o no
+# Entrada: Numero x
+# Salida:  El numero x es primo o no
+# Autor:   EALCALA
+# Fecha:  09.05.2017
+# Version: 1.0
+#Plataforma: Python v2.7
+
+x = input("Escoja un numero entero mayor o igual a 2 para saber si es primo o no: ")
+
+try:
+    x = int(x)
+except ValueError:
+    print("El valor no es un numero")
+
+div = [n for n in range(2,x) if x%n == 0]
+if len(div) == 0:
+    print("el numero es primo")
+else:
+    print("el numero no es primo")
